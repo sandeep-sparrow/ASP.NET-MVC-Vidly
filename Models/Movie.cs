664b35ba5f12c2bdff3990fb3cbfd357ecffb3f2
sandeep-sparrow <sandeep.p4856@gmail.com>
2021-08-05 01:41:09 +0530

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } // we give it couple of properties
+        
+
+    }
+
+    // let's say we want to create page where we randomly pick a movie
+    // assuming URL page is /movies/random
+}
